9d2f1d171f79305e1fb90875a6cbcea47b38ebdc
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-02-01 11:26:30 +0100

    doc: mouse designs eliminated

--- src/components/Loader/Loader.js
+++
@@ -1,7 +0,0 @@
-/* Debemos generar dos funciones:
-1º Una para que aparezca el loader. (hay que crear el div y toda la vaina).
-2º Otra para hacer que desaparezca.
-
-De esta forma podemos meterlo en diferentes partes de la web tan solo indicando
-que aparezca y se vaya cuando el contenido que queramos esté cargado y así
-evitamos tirones o saltos debido a la carga de las img o vids.*/
